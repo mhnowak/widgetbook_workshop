d73f3561737d651a95c7b8498d514d19ea8dcc1c
Michał Nowak <mhnowakk@gmail.com>
2024-09-03 16:35:07 +0200

    feat: new things

--- widgetbook/lib/ui/widgets/primary_button.dart
+++ widgetbook/lib/ui/widgets/primary_button.dart
@@ -1,9 +1,24 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:groceries_app/ui/ui.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
-/// Add Widgetbook use-case here
+@UseCase(
+  name: 'Default',
+  type: PrimaryButton,
+)
+Widget buildPrimaryButton(BuildContext context) {
+  return PrimaryButton(
+    content: context.knobs.list<String>(
+      label: 'text',
+      options: Style.values.map((val) => val.toString()).toList(),
+      labelBuilder: (style) => 'DUPA: $style',
+    ),
+    style: context.knobs.list(
+      label: 'Style',
+      options: Style.values,
+    ),
+    onPressed: () {},
+  );
+}
